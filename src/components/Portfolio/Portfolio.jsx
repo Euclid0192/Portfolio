b5040800db5cb72ac96ef8a23b2972e00f330729
Nam Nguyen <nguy1053@msu.edu>
2024-01-01 12:26:40 -0600

    Fixing links of projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
 import './Portfolio.css'
-import img from '../../assets/project.avif'
+import sudoku from '../../assets/sudoku.png'
+import chat from '../../assets/chat.png'
+import web from '../../assets/web.png'
+import demoSudoku from '../../assets/HalloweenSudoku.zip'
 
 const DATA = [
   {
     id: 1,
-    image: img,
+    image: sudoku,
     title: "Halloween-Spartan-themed Sudoku",
     github: "https://github.com/Euclid0192/Halloween-themed-Sudoku",
-    demo: ''
+    demo: demoSudoku,
   },
   {
     id: 2,
-    image: img,
+    image: chat,
     title: "ReTiMe - A Real-time Messaging App",
     github: "https://github.https://github.com/Euclid0192/Spartahack-8-Project",
-    demo: ''
+    demo: 'https://drive.google.com/file/d/12onKpfCDpIxvy038rHxENHKePmk5u9tv/view?usp=sharing'
   },
   {
     id: 3,
-    image: img,
+    image: web,
     title: "My Portfolio",
     github: "https://github.com/Euclid0192/Portfolio",
-    demo: ''
+    demo: 'https://my-portfolio-8bhz.onrender.com'
   },
 ]
 
@@ -43,7 +46,11 @@ const Portfolio = () => {
                 <h3>{title}</h3>
                 <div className='portfolio__item-cta'>
                   <a href={github} className='btn' target='_blank' rel="noopener noreferrer">Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank' rel="noopener noreferrer">Demo</a>            
+                  {
+                    id === 1 ? 
+                    <a href={demo} download className='btn btn-primary' target='_blank' rel="noopener noreferrer">Demo</a> :
+                    <a href={demo} className='btn btn-primary' target='_blank' rel="noopener noreferrer">Demo</a>      
+                  }
                 </div>
               </article>              
             )
