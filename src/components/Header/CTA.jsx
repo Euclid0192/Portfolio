aecccfa7dc45319ea7c0a0462da522cff5d7b861
Nam Nguyen <nguy1053@msu.edu>
2024-10-30 10:55:49 -0400

    Update resume with link Google Drive

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import resume from '../../assets/Resume.pdf'
 
 /// Call-to-action Buttons: buttons that make immediate responses
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={resume} download className='btn'>Download Resume</a>
+        <a href={"https://drive.google.com/file/d/1tv_QtaI66MW9hvZfQ8NY6L9Io01jtrdH/view?usp=sharing"} download className='btn'>Download Resume</a>
         <a href='#contact' className='btn btn-primary'>Contact me!</a>
     </div>
   )
