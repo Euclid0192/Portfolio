9547b166ce5491efa445f81be437c351850feac5
Nam Nguyen <nguy1053@msu.edu>
2025-02-24 20:56:02 -0500

    Add new Golang project

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import './Portfolio.css'
-import chatImg from '../../assets/chat-img.png'
-import sudokuImg from '../../assets/halloween-sudoku.png'
-import flowImg from '../../assets/flow.png'
 import sudoku from '../../assets/HalloweenSudoku.zip'
 import portfolioGif from '../../assets/Portfolio.gif'
 import workflowGif from '../../assets/workflow.gif'
 import chatGif from '../../assets/chat.gif'
 import halloweenGif from '../../assets/pumpkin.gif'
 import peaceGif from '../../assets/peace.gif'
+import orderGif from '../../assets/order.gif'
 
 const DATA = [
   {
@@ -20,34 +18,39 @@ const DATA = [
   },
   {
     id: 1,
+    image: orderGif,
+    title: "Order-On-the-Go!",
+    github: "https://github.com/Euclid0192/golang-microservices",
+    demo: ''
+  },
+  {
+    id: 2,
     image: workflowGif,
     title: "CourseFlow - Build your 4-year plan",
     github: "https://github.com/Euclid0192/CourseFlowFE",
     demo: "https://courseflowfe.onrender.com/"
   },
   {
-    id: 2,
+    id: 3,
     image: halloweenGif,
     title: "Halloween-Spartan-themed Sudoku",
     github: "https://github.com/Euclid0192/Halloween-themed-Sudoku",
-    // demo: "https://drive.google.com/file/d/1Usk8-ST2rSh8kcY8weXB_n_k-1N1tuzm/view?usp=drive_link"
     demo: sudoku
   },
   {
-    id: 3,
+    id: 4,
     image: portfolioGif,
     title: "My portfolio",
     github: "https://github.com/Euclid0192/Portfolio",
     demo: 'http://bit.ly/portfolio-euclid'
   },
   {
-    id: 4,
+    id: 5,
     image: chatGif,
     title: "ReTiMe - A Real-time Messaging App",
     github: "https://github.com/Euclid0192/ReTiMe",
     demo: 'https://drive.google.com/file/d/12onKpfCDpIxvy038rHxENHKePmk5u9tv/view?usp=sharing'
-  },
-
+  }
 ]
 
 const Portfolio = () => {
